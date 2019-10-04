e9e793cbfc88b438e08da48a4805ebe8d2431371
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-10-04 14:00:45 +0500

    .

---
+++ app.py
@@ -0,0 +1,11 @@
+# Elipe - Efficient Image Processing Engine
+
+# Elipe Homepage:               https://elipe.io
+# Elipe App:                    https://app.elipe.io  &  https://elipeapp.com
+# Elipe CDN:                    https://cdn.elipe.io  &  https://elipecdn.com
+# Elipe API:                    https://api.elipe.io  &  https://elipeapi.com
+
+# e.g https://cdn.elipe.io/$workspace/$res/$file_name.$image_ext
+
+#     https://cdn.elipe.io/wixware/200x300/sample.png
+#     https://elipecdn.com/wixware/200x300/sample.png
